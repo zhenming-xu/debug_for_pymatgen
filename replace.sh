bded4eb63eddfcd70fb5873d4057b4f95139f3da
zhenming-xu <15216105346@163.com>
2021-01-10 21:39:37 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,7 +1,7 @@
-mv diffusion_analyzer.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
-mv computed_entries.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/entries/
-mv inputs.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
-mv sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
-mv plotter.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/electronic_structure/
-mv func_groups.json  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
-mv wulff.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
+cp diffusion_analyzer.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
+cp computed_entries.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/entries/
+cp inputs.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+cp sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+cp plotter.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/electronic_structure/
+cp func_groups.json  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
+cp wulff.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
