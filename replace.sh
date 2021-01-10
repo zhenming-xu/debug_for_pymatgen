69e247f80723d80c688115186213475b141d9099
zhenming-xu <15216105346@163.com>
2021-01-10 21:14:48 +0800

    Add files via upload

---
+++ replace.sh
@@ -0,0 +1,6 @@
+mv diffusion_analyzer.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
+mv inputs.py sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+mv sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+mv plotter.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/electronic_structure/
+mv func_groups.json  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
+mv wulff.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
