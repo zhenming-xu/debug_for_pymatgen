3374178b6dc07f89d060666ead6c4c1971837b3f
zhenming-xu <15216105346@163.com>
2021-01-10 21:15:56 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,5 +1,5 @@
 mv diffusion_analyzer.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
-mv inputs.py sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+mv inputs.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
 mv sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
 mv plotter.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/electronic_structure/
 mv func_groups.json  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
