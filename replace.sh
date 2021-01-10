71181218ba11eadf8bbedd2aa5f7b28acf19b80d
zhenming-xu <15216105346@163.com>
2021-01-10 21:40:19 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -5,3 +5,4 @@ cp sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
 cp plotter.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/electronic_structure/
 cp func_groups.json  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
 cp wulff.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
+echo 'copy file done!'
