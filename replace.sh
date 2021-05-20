6793e3b9d442a3e885f755efe1e45d3b53b0dadc
zhenming-xu <15216105346@163.com>
2021-05-20 22:55:13 +0800

    Add files via upload

---
+++ replace.sh
@@ -0,0 +1,23 @@
+cp diffusion_analyzer.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
+cp computed_entries.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/entries/
+cp inputs.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+cp sets.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/io/vasp/
+cp plotter.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/electronic_structure/
+cp func_groups.json  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
+cp wulff.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/
+cp thermodynamics.py /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/defects/
+cp core.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/analysis/defects/
+cp units.py  /Users/xzm/anaconda3/lib/python3.7/site-packages/pymatgen/core/
+
+echo '             \\\///           '
+echo '            / _  _ \          '
+echo '          (| (o)(o) |)        '
+echo '  +-----.OOOo--()--oOOO.-----+'
+echo '  |                          |'
+echo '  |     copy file done!      |'
+echo '  |                          |'
+echo '  +-----.oooO----------------+'
+echo '         (   )   Oooo.        '
+echo '         \ (    (   )         '
+echo '          \_)    ) /          '
+echo '                (_/           '
